7d5300003e45bcc7c0ca51b6790adbba69b056e5
Ivan-C77 <ivgc2088@gmail.com>
2024-11-11 17:42:56 -0300

    atualização Desafio do Felipão

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-let nickname = "Elandor"
-let xpDoJogador = 8.777
-let ranking
+// Variáveis indicando nomes
+let nickName = "Elandor"
+let xpDoJogador = 8.777;
 
-if(xpDoJogador >= 0 && xpDoJogador <= 1.000){ranking = "Ferro!";
-} else if(xpDoJogador >= 1.001 && xpDoJogador <= 2.000){ranking = "Bronze!"
-} else if(xpDoJogador >= 2.001 && xpDoJogador <= 5.000){ranking = "Prata!"
-} else if(xpDoJogador >= 5.001 && xpDoJogador <= 7.000){ranking = "Ouro!"
-} else if(xpDoJogador >= 7.001 && xpDoJogador <= 8.000){ranking = "Platina!"
-} else if(xpDoJogador >= 8.001 && xpDoJogador <= 9.000){ranking = "Ascendente!"
-} else if(xpDoJogador >= 9.001 && xpDoJogador <= 10.000){ranking = "Imortal!"
-} else {ranking = "Radiante!"}
+// Usando array de objetos e não matriz, para indicar as classificações
+let rankings = [
+  { xp: [0, 1.000], ranking: "Ferro!" },
+  { xp: [1.001, 2.000], ranking: "Bronze!" },
+  { xp: [2.001, 5.000], ranking: "Prata!" },
+  { xp: [5.001, 7.000], ranking: "Ouro!" },
+  { xp: [7.001, 8.000], ranking: "Platina!" },
+  { xp: [8.001, 9.000], ranking: "Ascendente!"},
+  { xp: [9.001, 10.000], ranking: "Imortal!"},
+  { xp: [10.001, 12.000], ranking: "Radiante!"}
 
-console.log("O Heroi de nome " + nickname, "está no nível de " + ranking)
+];
+
+// Loop para verificar em qual linha xpDoJogador se encaixa
+for (let i = 0; i < rankings.length; i++) {
+  if (xpDoJogador >= rankings[i].xp[0] && xpDoJogador <= rankings[i].xp[1]) {
+    ranking = rankings[i].ranking;
+    break; // Interrompe o loop assim que encontrar o ranking
+  }
+}
+// Concatenando o nome + o ranking encontrado
+console.log("O Heroi de nome " + nickName, "está no nível de " + ranking);
